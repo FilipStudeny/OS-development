7d3cd61a3f8e6789c07d529faf89d17060d3d773
FilipStudeny <studenyfilip@gmail.com>
2025-12-23 20:05:32 +0100

    Second boot

--- boot.asm
+++ boot.asm
@@ -1,48 +1,46 @@
-[BITS 16]              ; 16-bit real mode
-[ORG 0x7c00]           ; BIOS loads boot sector here
+[BITS 16]
+[ORG 0]
+
+jmp 0x7C0:start     ; Set CS
 
 start:
-    xor ax, ax
+    cli             ; Disable interrupts during setup
+
+    mov ax, 0x7C0
     mov ds, ax
     mov es, ax
-    mov ss, ax
-    mov sp, 0x7c00
 
-PrintText:
-    mov ah, 0x13       ; BIOS: write string
-    mov al, 1          ; update cursor
-    mov bx, 0x000A     ; page 0, light green text
-    xor dx, dx         ; row 0, column 0
-    mov bp, Message    ; address of string
-    mov cx, MessageLen ; string length
-    int 0x10
+    xor ax, ax
+    mov ss, ax
+    mov sp, 0x7C00  ; Stack grows downward from here
+
+    sti             ; Enable interrupts (REQUIRED for HLT)
+
+    mov si, message
+    call print
 
 End:
-    hlt
-    jmp End            ; infinite loop
+    hlt             ; Sleep until next interrupt
+    jmp End         ; Resume sleep again
 
-Message:
-    db "Hello"
+; Print null-terminated string
+print:
+    lodsb
+    test al, al
+    jz .done
+    call printCharacter
+    jmp print
 
-MessageLen equ $ - Message
+.done:
+    ret
 
+printCharacter:
+    mov ah, 0x0E
+    int 0x10
+    ret
 
-times (0x1BE - ($ - $$)) db 0
+message:
+    db 'Hello world!', 0
 
-; Partition table entry (16 bytes)
-
-    db 80h             ; bootable partition
-    db 0, 2, 0         ; starting CHS
-    db 0F0h            ; partition type (placeholder)
-    db 0FFh, 0FFh, 0FFh ; ending CHS
-    dd 1               ; starting LBA
-    dd (20*16*63 - 1)  ; number of sectors
-
-times (16*3) db 0      ; remaining partition entries
-
-; -------------------------------------------------
-; Boot signature
-; -------------------------------------------------
-
-    db 0x55
-    db 0xAA
+times 510 - ($ - $$) db 0
+dw 0xAA55
